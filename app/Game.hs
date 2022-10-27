df9d0269690f242f2b7c7e80681f502d5e0fcb0c
jorge.orosan <jorge.orosan@gmail.com>
2022-10-27 11:50:57 -0600

    Validate if deck of cards is empty

--- app/Game.hs
+++ app/Game.hs
@@ -5,12 +5,6 @@ import Control.Lens (element)
 
 import Types
 
-validCard :: Maybe Card -> Card -> Bool
-validCard Nothing c2 = True
-validCard (Just c1) c2 =
-  color  c1 == color  c2 ||
-  number c1 == number c2
-
 startGame :: [Card] -> [Card] -> Users -> IO ()
 startGame unusedCards playedCards users = do
   putStrLn ""
@@ -19,44 +13,52 @@ startGame unusedCards playedCards users = do
   putStrLn "--------------------------------------------------------"
   userPlay unusedCards playedCards users 0
 
-getPosition :: Int -> Int -> Int
-getPosition c t = if c < t - 1 then c + 1 else 0
-
-lastCardPlayed :: [Card] -> Maybe Card
-lastCardPlayed [] = Nothing
-lastCardPlayed (c : cs) = Just c
-
 userPlay :: [Card] -> [Card] -> Users -> Int -> IO ()
 userPlay unusedCards playedCards users pos = do
   putStrLn ""
   putStrLn $ "User " ++ show pos
-  -- putStrLn $ "Unused cards: " ++ show unusedCards
   putStrLn $ "Last played card: " ++ show (lastCardPlayed playedCards)
+
   let currentUser = users !! pos
+
   putStr "Your cards: "
   print $ userCards currentUser
   putStrLn "Enter your card to play or T to take one card or Q to quit: "
   input <- getLine
+
   case input of
     [c, n] -> do
+
       case fromCharToColor c of
         Just color -> do
-          let cardPlayed = Card { color = color, number  = read [n] }
-              cardExist = cardPlayed `elem` userCards currentUser
-          if cardExist && validCard (lastCardPlayed playedCards) cardPlayed
+
+          let playedCard = Card { color = color, number  = read [n] }
+              cardExists = playedCard `elem` userCards currentUser
+              validCard' = validCard (lastCardPlayed playedCards) playedCard
+
+          if cardExists && validCard'
             then do
-              let updatedUsers = updateUserCards users pos Play cardPlayed
+
+              let updatedUsers = updateUserCards users pos Play playedCard
+                  playedCards' = playedCard : playedCards
+                  newPosition  = getPosition pos (length users)
+
               if null (userCards $ updatedUsers !! pos)
                 then putStrLn "¡¡¡YOU WIN!!!"
-                else userPlay unusedCards (cardPlayed : playedCards) updatedUsers $ getPosition pos (length users)
+                else userPlay unusedCards playedCards' updatedUsers newPosition
+
             else tryAgain
         _    -> tryAgain
+
     "Q"      -> putStrLn "Exit game"
     "T"      -> do
-      let takenCard = head unusedCards
+      let unusedCards' = if null unusedCards then reverse $ tail playedCards else unusedCards
+          playedCards' = if null unusedCards then [head playedCards] else playedCards
+          takenCard    = head unusedCards'
           updatedUsers = updateUserCards users pos Take takenCard
-      userPlay (drop 1 unusedCards) playedCards updatedUsers pos
+      userPlay (drop 1 unusedCards') playedCards' updatedUsers pos
     _        -> tryAgain
+
   where
     tryAgain :: IO ()
     tryAgain = do
@@ -67,3 +69,16 @@ userPlay unusedCards playedCards users pos = do
     updateUserCards users pos a card
       | a == Take = users & element pos .~ User { pos = pos, userCards = card : userCards (users !! pos) }
       | a == Play = users & element pos .~ User { pos = pos, userCards =  filter (/= card) $ userCards (users !! pos) }
+
+validCard :: Maybe Card -> Card -> Bool
+validCard Nothing c2 = True
+validCard (Just c1) c2 =
+  color  c1 == color  c2 ||
+  number c1 == number c2
+
+getPosition :: Int -> Int -> Int
+getPosition c t = if c < t - 1 then c + 1 else 0
+
+lastCardPlayed :: [Card] -> Maybe Card
+lastCardPlayed [] = Nothing
+lastCardPlayed (c : cs) = Just c
